c35fa131e649adc6998ed749ed911e3fbcecdd71
kvschaitanya <kvschaitanyanalluri25@gmail.com>
2026-05-20 22:49:46 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
 
-    let mut command: String = String::new();
-    io::stdin()
-        .read_line(&mut command)
-        .expect("Couldn't read the input");
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    println!("{}: command not found", command.trim());
+        let mut command: String = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Couldn't read the input");
+
+        println!("{}: command not found", command.trim());
+    }
 }
